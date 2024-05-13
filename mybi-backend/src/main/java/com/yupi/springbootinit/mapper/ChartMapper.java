e9bd382e5a7fa2ee63da060b00a0a57ace1baee6
carriemint <863162796@qq.com>
2024-05-13 18:53:03 +0800

    crud_init

---
+++ mybi-backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author carriemint
+* @description 针对表【chart(图表信息)】的数据库操作Mapper
+* @createDate 2024-05-13 18:08:53
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
